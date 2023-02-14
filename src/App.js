b41a9ec40879b4d5c0d1c3563bd082398a67f597
Duy Hieu <buihieuduya4@gmail.com>
2023-02-15 02:10:48 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navigation from "./components/Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import {Route, Routes } from "react-router-dom"; // import BrowserRouter, Switch and Route from react-router-dom
+import Contact from "./components/Contact"; // import the Contact component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Navigation />
+        <Routes> 
+          <Route path="/" element={<Main/>} />
+          <Route path="/contact" element={<Contact/>}  /> 
+          </Routes>
+        <Footer/>
+      </div>
+
   );
 }
 
---
+++ src/shared under/ListOfFilms.js
@@ -0,0 +1,90 @@
+export const Films = [
+    {
+        id: 1,
+        img: 'assets/images/thukikim.jpg',
+        title: 'What is Wrong, Secretary Kim',
+        year: '2018',
+        nation: 'Korea',
+        desc: 'abc'
+    },
+
+    {
+        id: 2,
+        img: 'assets/images/BP.jpg',
+        title: 'Business Proposal',
+        year: '2022',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 3,
+        img: 'assets/images/dubaotinhyeuvathoitiet.jpg',
+        title: 'Forecasting love and Weather',
+        year: '2022',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 4,
+        img: 'assets/images/hacanhnoianh.jpg',
+        title: 'Crash landing on you',
+        year: '2019',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 5,
+        img: 'assets/images/itaewonclass.jpg',
+        title: 'Itaewon class',
+        year: '2020',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 6,
+        img: 'assets/images/parasite.jpg',
+        title: 'Parasite',
+        year: '2019',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 7,
+        img: 'assets/images/penthouse1.jpg',
+        title: 'The Penthouse: War in Life',
+        year: '2020',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 8,
+        img: 'assets/images/squidgame.jpg',
+        title: 'Squid Games',
+        year: '2021',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+
+    {
+        id: 9,
+        img: 'assets/images/tuoi25tuoi21.jpg',
+        title: 'Twenty-Five Twenty-One',
+        year: '2022',
+        nation: 'Korea',
+        desc: 'abc'
+
+    },
+]
